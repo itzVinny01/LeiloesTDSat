d696718ded18b8ea24660734230ff9c9f3acfdbf
Vinicius Renan <fznnn123@hotmail.com>
2024-06-21 20:25:31 -0300

    botão de salvar cadastrando produto.

--- src/ProdutosDAO.java
+++ src/ProdutosDAO.java
@@ -8,11 +8,15 @@
  * @author Adm
  */
 
+import java.lang.System.Logger;
+import java.lang.System.Logger.Level;
 import java.sql.PreparedStatement;
 import java.sql.Connection;
 import javax.swing.JOptionPane;
 import java.sql.ResultSet;
 import java.util.ArrayList;
+import java.sql.SQLException;
+import java.util.List;
 
 
 public class ProdutosDAO {
@@ -20,23 +24,28 @@ public class ProdutosDAO {
     Connection conn;
     PreparedStatement prep;
     ResultSet resultset;
-    ArrayList<ProdutosDTO> listagem = new ArrayList<>();
+    List<ProdutosDTO> listagem = new ArrayList<>();
     
     public void cadastrarProduto (ProdutosDTO produto){
-        
-        
-        //conn = new conectaDAO().connectDB();
-        
-        
+        String sql = "INSERT INTO produtos (nome, valor, status) VALUES (?, ?, ?)";
+        try(Connection conn = new conectaDAO().connectDB();
+                PreparedStatement pstmt = conn.prepareStatement(sql)){
+            
+            pstmt.setString(1, produto.getNome());
+            pstmt.setInt(2, produto.getValor());
+            pstmt.setString(3, produto.getStatus());
+            pstmt.executeUpdate();
+            JOptionPane.showMessageDialog(null, "Cadastro realizado com sucesso.");
+            
+        }catch (SQLException ex) {
+            JOptionPane.showMessageDialog(null, "Não foi possível realizar o cadastro!"
+                                                + "\n Tente Novamente.");
+        }
     }
     
-    public ArrayList<ProdutosDTO> listarProdutos(){
-        
+    public List<ProdutosDTO> listarProdutos(){
+
         return listagem;
-    }
-    
-    
-    
-        
+    }   
 }
 
