01dd1d2074dd7aacdc3752571df4f6af0a5f91ba
Vinicius Renan <fznnn123@hotmail.com>
2024-06-27 19:51:14 -0300

    Correção de bugs

--- src/ProdutosDAO.java
+++ src/ProdutosDAO.java
@@ -8,23 +8,23 @@
  * @author Adm
  */
 
-import java.lang.System.Logger;
-import java.lang.System.Logger.Level;
 import java.sql.PreparedStatement;
 import java.sql.Connection;
 import javax.swing.JOptionPane;
 import java.sql.ResultSet;
-import java.util.ArrayList;
 import java.sql.SQLException;
+import java.util.ArrayList;
 import java.util.List;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 
 public class ProdutosDAO {
     
-    Connection conn;
+    //Connection conn;
     PreparedStatement prep;
     ResultSet resultset;
-    List<ProdutosDTO> listagem = new ArrayList<>();
+    ArrayList<ProdutosDTO> listagem = new ArrayList<>();
     
     public void cadastrarProduto (ProdutosDTO produto){
         String sql = "INSERT INTO produtos (nome, valor, status) VALUES (?, ?, ?)";
@@ -38,6 +38,7 @@ public class ProdutosDAO {
             JOptionPane.showMessageDialog(null, "Cadastro realizado com sucesso.");
             
         }catch (SQLException ex) {
+            Logger.getLogger(ProdutosDAO.class.getName()).log(Level.SEVERE, null, ex);
             JOptionPane.showMessageDialog(null, "Não foi possível realizar o cadastro!"
                                                 + "\n Tente Novamente.");
         }
@@ -59,44 +60,7 @@ public class ProdutosDAO {
                 listagem.add(produto);
             }
         } catch (SQLException ex) {
-            JOptionPane.showMessageDialog(null, "Não foi possível listar os produtos.");
-        }
-        
-       return listagem;
-    }
-    
-    public void venderProduto(int id) {
-        String sql = "UPDATE produtos SET status = 'vendido' WHERE id = ?";
-        
-        try(Connection conn = new conectaDAO().connectDB();
-            PreparedStatement pstmt = conn.prepareStatement(sql)){
-            
-            pstmt.setInt(1, id);
-            pstmt.executeUpdate();
-            
-            JOptionPane.showMessageDialog(null, "Lista autalizada com sucesso!");
-        } catch (SQLException ex) {
-            JOptionPane.showMessageDialog(null, "Não foi possivel atualizar status de compra.");
-        }
-    }
-    
-    public List<ProdutosDTO> listarProdutosVendidos(){
-        String sql = "SELECT * FROM produtos WHERE status = 'vendido'";
-        
-        try (Connection conn = new conectaDAO().connectDB();
-             PreparedStatement pstmt = conn.prepareStatement(sql);
-             ResultSet resultset = pstmt.executeQuery()) {
-            
-            while (resultset.next()) {
-                ProdutosDTO produto = new ProdutosDTO();
-                produto.setId(resultset.getInt("id"));
-                produto.setNome(resultset.getString("nome"));
-                produto.setValor(resultset.getInt("valor"));
-                produto.setStatus(resultset.getString("status"));
-                listagem.add(produto);
-            }
-        } catch (SQLException ex) {
-            System.out.println(ex.getMessage());
+            Logger.getLogger(ProdutosDAO.class.getName()).log(Level.SEVERE, null, ex);
         }
         
        return listagem;
